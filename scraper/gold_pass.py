c4276c2c79c99f18d8baf54e70c7ed1c7f04e69a
zengfung <zengfung.liew@gmail.com>
2022-12-26 10:39:46 -0800

    Clean up StorageHandler & GoldPassHandler

--- scraper/gold_pass.py
+++ scraper/gold_pass.py
@@ -2,11 +2,11 @@ import logging
 import coc
 import datetime
 
-from typing import Dict
-from typing import Union
-from typing import Generator
+from collections.abc import Iterator
 from scraper import CONFIG
 from scraper.storage import StorageHandler
+from scraper.utils import try_get_attr
+from azure.data.tables import TableEntity
 
 LOGGER = logging.getLogger(__name__)
     
@@ -17,21 +17,14 @@ class GoldPassTableHandler(StorageHandler):
 
     Attributes
     ----------
-    coc_client : coc.Client
-        The client used to access the Clash of Clans API.
-    table : str
-        The name of the table.
     scrape_enabled : bool
         Determines if data scraping should be performed.
+    abandon_scrape_if_entity_exists : bool
+        Determines if the scrape should be abandoned if the entity exists in
+        the table.
     
     Methods
     -------
-    __convert_timedelta_to_days__(dt: datetime.timedelta) -> float
-        Converts the given timedelta to days.
-    __convert_data_to_entity_list__(data: coc) -> Generator[Dict[str,Union[float,int,str]],None,None]
-        Converts the given data to an enumerable of entities.
-    __update_table__() -> None
-        Updates the table with the current Gold Pass Season data.
     process_table() -> None
         Public API to update the table with the current Gold Pass Season data.
     """
@@ -44,13 +37,13 @@ class GoldPassTableHandler(StorageHandler):
         """
         Parameters
         ----------
-        coc_client : coc.Client
-            The client used to access the Clash of Clans API.
+        **kwargs
+            Keyword arguments to pass to the StorageHandler class.
         """
 
         super().__init__(table_name=self.configs['TableName'], **kwargs)
 
-    def __does_entity_exist__(self) -> bool:
+    def __does_entity_exist(self) -> bool:
         """
         Determines if the entity exists in the table.
 
@@ -61,12 +54,12 @@ class GoldPassTableHandler(StorageHandler):
         """
 
         LOGGER.debug(f'Checking if entity exists in table {self.table_name}.')
-        partition_key = self.__get_partition_key__()
-        row_key = self.__get_row_key__()
-        entity = self.try_get_entity(partition_key, row_key, select='PartitionKey')
+        partition_key = self.__get_partition_key()
+        row_key = self.__get_row_key()
+        entity = self.try_get_entity(partition_key, row_key, select='PartitionKey', retries_remaining=self.retry_entity_extraction_count)
         return entity is not None
 
-    def __convert_timedelta_to_days__(self, dt: datetime.timedelta) -> float:
+    def __convert_timedelta_to_days(self, dt: datetime.timedelta) -> float:
         """
         Converts the given timedelta to days.
 
@@ -83,7 +76,7 @@ class GoldPassTableHandler(StorageHandler):
 
         return dt / datetime.timedelta(days=1)
 
-    def __get_partition_key__(self) -> str:
+    def __get_partition_key(self) -> str:
         """
         Gets the partition key for the current month, i.e. the current year.
 
@@ -95,7 +88,7 @@ class GoldPassTableHandler(StorageHandler):
 
         return datetime.datetime.now().strftime('%Y')
 
-    def __get_row_key__(self) -> str:
+    def __get_row_key(self) -> str:
         """
         Gets the row key for the current month, i.e. the current month.
 
@@ -107,49 +100,111 @@ class GoldPassTableHandler(StorageHandler):
 
         return datetime.datetime.now().strftime('%m')
 
-    def __convert_data_to_entity_list__(self, data: coc.miscmodels.GoldPassSeason) -> Generator[Dict[str,Union[float,int,str]],None,None]:
+    def __convert_data_to_entity_list(self, data: coc.miscmodels.GoldPassSeason) -> Iterator[TableEntity]:
         """
-        Converts the given data to an enumerable of entities.
+        Converts the given data to an iterable of entities.
 
         Parameters
         ----------
         data : coc.miscmodels.GoldPassSeason
             The data to convert.
 
-        Returns
-        -------
-        Generator[Dict[str,Union[float,int,str]],None,None]
+        Yields
+        ------
+        azure.data.tables.TableEntity
             The data converted to an enumerable of entities.
         """
 
         LOGGER.debug(f'Creating entity for Gold Pass Season.')
-        entity = dict()
-        entity['PartitionKey'] = self.__get_partition_key__()
-        entity['RowKey'] = self.__get_row_key__()
-        entity['SeasonId'] = datetime.datetime.now().strftime('%Y-%m')
-        entity['StartTime'] = data.start_time.time
-        entity['EndTime'] = data.end_time.time
-        entity['Duration'] = self.__convert_timedelta_to_days__(data.duration)
+        start_time = try_get_attr(data, 'start_time', default=None)
+        end_time = try_get_attr(data, 'end_time', default=None)
+        duration = try_get_attr(data, 'duration', default=None)
+
+        entity = TableEntity()
+        entity.PartitionKey = self.__get_partition_key()
+        entity.RowKey = self.__get_row_key()
+        entity.SeasonId = datetime.datetime.now().strftime('%Y%m')
+        entity.StartTime = start_time.time if start_time is not None else None
+        entity.EndTime = end_time.time if end_time is not None else None
+        entity.Duration = self.__convert_timedelta_to_days(duration) if duration is not None else None
         yield entity
 
-    async def __update_table__(self) -> None:
+    async def __get_data(self, retries_remaining: int = 0) -> coc.miscmodels.GoldPassSeason:
         """
-        Scrape and update the table with the current Gold Pass Season data.
+        Gets the current Gold Pass Season data.
+
+        Parameters
+        ----------
+        retries_remaining : int, optional
+            (Default: 0) The number of retries remaining.
+
+        Raises
+        ------
+        coc.errors.Forbidden
+            If the client is not authorized to access the data.
+
+        Returns
+        -------
+        coc.miscmodels.GoldPassSeason
+            The current Gold Pass Season data.
         """
 
-        should_abandon_scrape = self.abandon_scrape_if_entity_exists and self.__does_entity_exist__()
+        assert retries_remaining >= 0, 'retries_remaining must be greater than or equal to 0.'
+        try:
+            LOGGER.debug('Getting Gold Pass data.')
+            return await self.coc_client.get_current_goldpass_season()
+        except coc.errors.Forbidden as ex:
+            LOGGER.error(f'Encountered Forbidden error while getting Gold Pass data.')
+            
+            if retries_remaining > 0:
+                LOGGER.debug(f'Retrying getting Gold Pass data.  Retries remaining: {retries_remaining}.')
+                await self.restart_coc_client_session()
+                return await self.__get_data(retries_remaining-1)
+            else:
+                raise ex
+
+    async def __update_table(self) -> None:
+        """
+        Scrape and update the table with the current Gold Pass Season data.
+
+        Raises
+        ------
+        coc.errors.Forbidden
+            If the client is not authorized to access the data.
+
+        Returns
+        -------
+        None
+        """
+
+        should_abandon_scrape = self.abandon_scrape_if_entity_exists and self.__does_entity_exist()
         if should_abandon_scrape:
             LOGGER.info(f'Abandoning scrape because entity exists in table {self.table_name} and GoldPassSettings.AbandonScrapeIfEntityExists is {self.abandon_scrape_if_entity_exists}.')
             return None
 
         LOGGER.debug('Scraping Gold Pass data.')
-        data = await self.coc_client.get_current_goldpass_season()
-        entities = self.__convert_data_to_entity_list__(data)
+        data = await self.__get_data()
+        entities = self.__convert_data_to_entity_list(data)
         self.write_data_to_table(entities=entities)
 
     async def process_table(self, coc_client_handling: bool = True) -> None:
         """
         Public API to update the table with the current Gold Pass Season data.
+
+        Parameters
+        ----------
+        coc_client_handling : bool, optional
+            (Default: True) Whether or not to handle the coc client session
+            automatically.
+
+        Raises
+        ------
+        coc.errors.Forbidden
+            If the client is not authorized to access the data.
+
+        Returns
+        -------
+        None
         """
 
         if coc_client_handling:
@@ -158,7 +213,7 @@ class GoldPassTableHandler(StorageHandler):
         if self.scrape_enabled:
             try:
                 LOGGER.info(f'Gold Pass table {self.table_name} is updating.')
-                await self.__update_table__()
+                await self.__update_table()
             except Exception as ex:
                 LOGGER.error(f'Unable to update table with Gold Pass Season data.')
                 LOGGER.error(str(ex))
--- scraper/storage.py
+++ scraper/storage.py
@@ -2,12 +2,9 @@ import logging
 import coc
 
 from scraper import CONFIG
-from typing import Dict
-from typing import Union
-from typing import Optional
 from collections.abc import Iterable
 from azure.core.exceptions import ResourceExistsError
-from azure.core.exceptions import HttpResponseError
+from azure.core.exceptions import ResourceNotFoundError
 from azure.core.exceptions import ClientAuthenticationError
 from azure.core.credentials import AzureNamedKeyCredential
 from azure.data.tables import TableServiceClient
@@ -18,34 +15,66 @@ LOGGER = logging.getLogger(__name__)
 
 class StorageHandler(object):
     """
-    The storage handler object is used to connect to a table in Azure Table 
-    Storage. Data modifications on the corresponding table via inserting, 
-    upserting, and deleting entities are handled by this class.
+    The storage handler object is used to connect to the Clash of Clans API
+    client and a table in Azure Table Storage. Connection retries and data 
+    modifications on the corresponding table via inserting, upserting, and 
+    deleting entities are handled by this class.
 
     Attributes
     ----------
+    upsert_enabled : bool
+        A boolean value indicating whether or not upserting is enabled if 
+        the entity already exists in the table.
+    retry_entity_creation_enabled : bool
+        A boolean value indicating whether or not retrying entity creation
+        is enabled if a previous creation attempt has failed.
+    retry_entity_creation_count : int
+        The number of times to retry entity creation if a previous creation
+        attempt has failed.
+    retry_entity_extraction_enabled : bool
+        A boolean value indicating whether or not retrying entity extraction
+        is enabled if a previous extraction attempt has failed.
+    retry_entity_extraction_count : int
+        The number of times to retry entity extraction if a previous extraction
+        attempt has failed.
+    coc_client : coc.Client
+        The Clash of Clans API client object.
+    table_name : str
+        The name of the table in Azure Table Storage account.
     service_table_client : azure.data.tables.TableServiceClient
         The TableServiceClient object connected to the Azure Table Storage.
     table_client : azure.data.tables.TableClient
         The TableClient object connected to the table in Azure Table Storage.
-    upsert_enabled : bool
-        A boolean value indicating whether or not upserting is enabled if 
-        the entity already exists in the table.
 
     Methods
     -------
-    __connect_table_service_client__(account_name: str = None, access_key: str = None, connection_string: str = None) -> azure.data.tables.TableServiceClient
+    __connect_table_service_client(account_name: str = None, access_key: str = None, connection_string: str = None) -> azure.data.tables.TableServiceClient
         Connects to the TableServiceClient object in Azure Table Storage 
         based on the given credentials.
-    __connect_table_client__(name: str) -> azure.data.tables.TableClient
+    __connect_table_client(name: str) -> azure.data.tables.TableClient
         Connects to the TableClient object in Azure Table Storage based on
         the given table name.
-    write_data_to_table(entities: Dict[str,Union[float,int,str]]) -> None
+    __try_create_or_upsert_entity(entity: azure.data.tables.TableEntity, retries_remaining: int = 0) -> None
+        Attempts to create or upsert an entity in the table in Azure Table
+        Storage. If the entity already exists, it will be updated.
+    restart_coc_client_session() -> None
+        Restarts the Clash of Clans API client session.
+    close_coc_client_session() -> None
+        Closes the Clash of Clans API client session.
+    start_coc_client_session() -> None
+        Starts the Clash of Clans API client session.
+    write_data_to_table(entities: collections.abc.Iterable[azure.data.tables.TableEntity]) -> None
         Writes the given entities to the table in Azure Table Storage.
+    try_get_entity(partition_key: str, row_key: str, retries_remaining: int = 0, **kwargs) -> azure.data.tables.TableEntity
+        Attempts to get an entity from the table in Azure Table Storage.
     """
 
     configs = CONFIG['StorageHandlerSettings']
     upsert_enabled = configs['UpsertAtFailedPushEnabled']
+    retry_entity_creation_enabled = configs['RetryEntityCreationEnabled']
+    retry_entity_creation_count = configs['RetryEntityCreationCount']
+    retry_entity_extraction_enabled = configs['RetryEntityExtractionEnabled']
+    retry_entity_extraction_count = configs['RetryEntityExtractionCount']
 
     def __init__(
             self,
@@ -60,6 +89,10 @@ class StorageHandler(object):
         ----------
         table_name : str
             The name of the table in Azure Table Storage account.
+        coc_email : str
+            The email address of the Clash of Clans API account.
+        coc_password : str
+            The password of the Clash of Clans API account.
         account_name : str, optional
             (Default: None) The account name of the Azure Table Storage.
         access_key : str, optional
@@ -69,24 +102,25 @@ class StorageHandler(object):
         """
 
         # Clash of Clans API Client
-        self.coc_email = coc_email
-        self.coc_password = coc_password
+        self.__coc_email = coc_email
+        self.__coc_password = coc_password
         # Need to manually start coc_client session
         self.coc_client = None  
 
         # Azure Table Storage Client
         self.table_name = table_name
-        self.account_name = account_name
-        self.access_key = access_key
-        self.connection_string = connection_string
+        self.__account_name = account_name
+        self.__access_key = access_key
+        self.__connection_string = connection_string
 
-        self.service_table_client = self.__connect_table_service_client__(account_name=account_name, access_key=access_key, connection_string=connection_string)
-        self.table_client = self.__connect_table_client__(table_name)
+        self.service_table_client = self.__connect_table_service_client(account_name=account_name, access_key=access_key, connection_string=connection_string)
+        self.table_client = self.__connect_table_client(table_name)
 
-    def __connect_table_service_client__(self,
-                                         account_name: str = None,
-                                         access_key: str = None,
-                                         connection_string: str = None) -> TableServiceClient:
+    def __connect_table_service_client(
+            self,
+            account_name: str = None,
+            access_key: str = None,
+            connection_string: str = None) -> TableServiceClient:
         """
         Connects to the TableServiceClient object in Azure Table Storage
         based on the given credentials.
@@ -126,7 +160,7 @@ class StorageHandler(object):
             except:
                 LOGGER.error('Connection attempt via account name and access key failed.')
 
-    def __connect_table_client__(self, name:str) -> TableClient:
+    def __connect_table_client(self, name:str) -> TableClient:
         """
         Connects to the TableClient object in Azure Table Storage based on
         the given table name.
@@ -149,9 +183,64 @@ class StorageHandler(object):
             LOGGER.error(f'Failed to connect to table client {name}.')
             LOGGER.error(str(ex))
 
-    async def __restart_coc_client_session__(self) -> None:
+    def __try_create_or_upsert_entity(
+            self, 
+            entity: TableEntity, 
+            retries_remaining: int = 0) -> None:
+        """
+        Attempts to create or upsert the given entity in the table in Azure
+        Table Storage. If the entity already exists, it will be updated with
+        the new values. If the entity does not exist, it will be created.
+        
+        Parameters
+        ----------
+        entity : azure.data.tables.TableEntity
+            The entity to be created or upserted in the table in Azure Table Storage.
+        retries_remaining : int, optional
+            (Default: 0) The number of retries remaining to attempt to create
+            or upsert the entity in the table in Azure Table Storage.
+        
+        Returns
+        -------
+        None
+        """
+
+        assert retries_remaining >= 0, 'Retries remaining must be greater than or equal to 0.'
+
+        try:
+            self.table_client.create_entity(entity=entity)
+            LOGGER.debug('Successfully written entity to table.')
+        except ResourceExistsError:
+            LOGGER.warning(f'Entity {entity["PartitionKey"]} already exists in {self.table_client}.')
+
+            if self.upsert_enabled:
+                try:
+                    LOGGER.debug(f'Attempting upsert of entity {entity["PartitionKey"]} to {self.table_client}')
+                    self.table_client.upsert_entity(entity=entity)
+                    LOGGER.debug('Successfully upsert entity.')
+                except Exception as ex:
+                    LOGGER.error('Failed to upsert entity.')
+                    LOGGER.error(str(ex))
+        except ClientAuthenticationError as ex:
+            LOGGER.error('Client authentication error encountered, attempting re-login and retry entity creation.')
+            LOGGER.error(str(ex))
+            self.service_table_client = self.__connect_table_service_client(account_name=self.__account_name, access_key=self.__access_key, connection_string=self.__connection_string)
+            self.table_client = self.__connect_table_client(self.table_name)
+
+            if self.retry_entity_creation_enabled and retries_remaining > 0:
+                LOGGER.debug(f'Retrying entity creation {retries_remaining} more times.')
+                self.__try_create_or_upsert_entity(entity=entity, retries_remaining=retries_remaining-1)
+            else:
+                LOGGER.debug('Entity creation retry limit reached / Retry not enabled, skipping entity creation.')
+
+
+    async def restart_coc_client_session(self) -> None:
         """
         Restarts the Clash of Clans API client session.
+
+        Returns
+        -------
+        None
         """
 
         LOGGER.debug('Restarting Clash of Clans API client session.')
@@ -161,6 +250,10 @@ class StorageHandler(object):
     async def start_coc_client_session(self) -> None:
         """
         Starts a Clash of Clans API client session.
+
+        Returns
+        -------
+        None
         """
 
         if self.coc_client is not None:
@@ -170,7 +263,7 @@ class StorageHandler(object):
         try:
             LOGGER.debug('Starting Clash of Clans API client session.')
             self.coc_client = coc.Client(load_game_data=coc.LoadGameData(default=True))
-            await self.coc_client.login(email=self.coc_email, password=self.coc_password)
+            await self.coc_client.login(email=self.__coc_email, password=self.__coc_password)
         except Exception as ex:
             LOGGER.error('Failed to start Clash of Clans API client session.')
             LOGGER.error(str(ex))
@@ -178,6 +271,10 @@ class StorageHandler(object):
     async def close_coc_client_session(self) -> None:
         """
         Closes the Clash of Clans API client session.
+
+        Returns
+        -------
+        None
         """
 
         if self.coc_client is None:
@@ -188,40 +285,31 @@ class StorageHandler(object):
         await self.coc_client.close()
         self.coc_client = None 
 
-    def write_data_to_table(self, entities: Iterable[Dict[str,Union[float,int,str]]]) -> None:
+    def write_data_to_table(self, entities: Iterable[TableEntity]) -> None:
         """
         Writes the given entities to the table in Azure Table Storage.
 
         Parameters
         ----------
-        entities : Generator[Dict[str,Union[float,int,str]],None,None]
+        entities : collections.abc.Iterable[azure.data.tables.TableEntity]
             The entities to be written to the table in Azure Table Storage.
+
+        Returns
+        -------
+        None
         """
         
         LOGGER.debug(f'Writing entities to the table {self.table_client}.')
         for entity in entities:
-            try:
-                self.table_client.create_entity(entity=entity)
-                LOGGER.debug('Successfully written entity to table.')
-            except ResourceExistsError:
-                LOGGER.warning(f'Entity {entity["PartitionKey"]} already exists in {self.table_client}.')
-
-                if self.upsert_enabled:
-                    try:
-                        LOGGER.debug(f'Attempting upsert of entity {entity["PartitionKey"]} to {self.table_client}')
-                        self.table_client.upsert_entity(entity=entity)
-                        LOGGER.debug('Successfully upsert entity.')
-                    except Exception as ex:
-                        LOGGER.error('Failed to upsert entity.')
-                        LOGGER.error(str(ex))
-            except ClientAuthenticationError:
-                LOGGER.error('Client authentication error encountered, attempting login retry.')
-                self.service_table_client = self.__connect_table_service_client__(account_name=self.account_name, access_key=self.access_key, connection_string=self.connection_string)
-                self.table_client = self.__connect_table_client__(self.table_name)
-
+            self.__try_create_or_upsert_entity(entity=entity, retries_remaining=self.retry_entity_creation_count)
         LOGGER.debug('Sent all entities to table.')
 
-    def try_get_entity(self, partition_key, row_key, **kwargs) -> TableEntity:
+    def try_get_entity(
+            self, 
+            partition_key: str,
+            row_key: str,
+            retries_remaining: int = 0,
+            **kwargs) -> TableEntity:
         """
         Attempts to get the entity with the given partition key and row key.
 
@@ -231,18 +319,35 @@ class StorageHandler(object):
             The partition key of the entity to be retrieved.
         row_key : str
             The row key of the entity to be retrieved.
+        retries_remaining : int, optional
+            (Default: 0) The number of retries remaining to attempt to get
+            the entity with the given partition key and row key.
+        **kwargs
+            Additional keyword arguments to pass to the get_entity method.
 
         Returns
         -------
-        TableEntity
+        azure.data.tables.TableEntity
             The entity with the given partition key and row key.
         """
+
+        assert retries_remaining >= 0, 'Retries remaining must be greater than or equal to 0.'
 
         try:
             LOGGER.debug(f'Attempting to get entity with partition key {partition_key} and row key {row_key}.')
             return self.table_client.get_entity(partition_key=partition_key, row_key=row_key, **kwargs)
-        except HttpResponseError as ex:
-            LOGGER.error(f'Entity with partition key {partition_key} and row key {row_key} not found.')
-            LOGGER.error(str(ex))
+        except ResourceNotFoundError as ex:
+            LOGGER.debug(f'Entity with partition key {partition_key} and row key {row_key} not found.')
+            LOGGER.debug(str(ex))
             return None
-    
+        except ClientAuthenticationError as ex:
+            LOGGER.error('Client authentication error encountered, attempting re-login and retry entity extraction.')
+            LOGGER.error(str(ex))
+            self.service_table_client = self.__connect_table_service_client(account_name=self.__account_name, access_key=self.__access_key, connection_string=self.__connection_string)
+            self.table_client = self.__connect_table_client(self.table_name)
+
+            if self.retry_entity_extraction_enabled and retries_remaining > 0:
+                LOGGER.debug(f'Retrying entity extraction {retries_remaining} more times.')
+                self.try_get_entity(partition_key=partition_key, row_key=row_key, retries_remaining=retries_remaining-1, **kwargs)
+            else:
+                LOGGER.debug('Entity extraction retry limit reached / Retry not enabled, skipping entity creation.')
